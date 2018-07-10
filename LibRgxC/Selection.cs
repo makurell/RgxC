231bfba7a739eb420e3fd4c53fceb3e4637c6fc1
starryyy <sianochara@gmail.com>
2018-07-10 20:29:08 +0100

    Begin Implement New Selection

--- LibRgxC/Selection.cs
+++ LibRgxC/Selection.cs
@@ -1,5 +1,4 @@
-﻿using LibRgxC.Exceptions;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,109 +8,72 @@ namespace LibRgxC
 {
     public class Selection
     {
-        public string Value = String.Empty;
+        public string Value = string.Empty;
         public Selection Parent = null;
+        public List<Selection> Children = new List<Selection>();
 
-        private int _thisoff = 0;
-        private int[] _offmap = null;
+        internal int _off = 0;
+        internal int _len = 0;
 
         public Selection(string value)
         {
             this.Value = value;
-            Init();
+            this._len = value.Length;
         }
 
-        internal Selection(string value, Selection parent, int off)
-        {
-            this.Value = value;
-            this.Parent = parent;
-            this._thisoff = off;
-            Init();
-        }
-
-        private void Init()
-        {
-            if (_offmap == null)
-            {
-                _offmap = new int[Value.Length];
-                //_offmap = new int[Value.Length].ToList();
-            }
-        }
-
-        //public int GetAbs(int off)
+        //public int GetAbs()
         //{
-        //    int ret = _thisoff + this._offmap[off];
-        //    Selection pselection = this.Parent;
-        //    while (pselection != null)
-        //    {
-        //        ret -= pselection._offmap[ret] - pselection._thisoff;
-        //        pselection = pselection.Parent;
-        //    }
-        //    return ret;
+
         //}
-
-        internal void Replace(int off, int len, string repl)
+        public List<Selection> GetAfter(int index)
         {
-            int vlen = Value.Length;
-            int mapoff = 0;
-            try
+            List<Selection> ret = new List<Selection>();
+            foreach(Selection child in Children)
             {
-                mapoff = this._offmap[off];
+                if (child._off >= index)
+                {
+                    ret.Add(child);
+                }
             }
-            catch (IndexOutOfRangeException ex)
-            {
-                //int neededno = (off - _offmap.Count);
-                //for (int i = 0; i < neededno; i++)
-                //{
-                //    _offmap.Add(0);
-                //}
-                throw new ReplacementException(ex);
-            }
-            int curoff = mapoff + off;//was err here
+            return ret;
+        }
 
-            Value = Value.Remove(curoff, len);
-            Value = Value.Insert(curoff, repl);
-
-            int offincr = repl.Length - len;
-            for(int i = curoff+1; i < _offmap.Length; i++)
-            {
-                _offmap[i] += offincr;
-            }
-
-            if(Parent != null)
-            {
-                Parent.Replace(_thisoff, vlen, Value);
-            }
+        public Selection Sel(int off, int len)
+        {
+            Selection ret = new Selection(Value.Substring(off, len));
+            ret._off = off;
+            ret.Parent = this;
+            this.Children.Add(ret);
+            return ret;
         }
 
         public void Replace(string repl)
         {
-            int len = Value.Length;
+            //actual str manip
             Value = repl;
-            if(Parent != null)
-            {
-                Parent.Replace(_thisoff, len, Value);
-            }
-        }
 
-        public Selection Sel(int start, int count)
-        {
-            string selval = Value.Substring(start, count);
-            int off = 0; //was err here
-            try
+            int q = repl.Length - this._len;
+            if(this.Parent != null)
             {
-                off = this._offmap[start];
-            }
-            catch (IndexOutOfRangeException ex)
-            {
-                //int neededno = (start - _offmap.Count);
-                //for (int i = 0; i < neededno; i++)
+                foreach(Selection sel in this.Parent.GetAfter(this._off + this._len))
+                {
+                    sel._off += q;
+                }
+                string parentval = this.Parent.Value.Remove(this._off, this._len).Insert(this._off, repl);
+                this.Parent.Replace(parentval);
+                //this.Parent._len += q;
+
+                //Selection curParent = this.Parent;
+                //int curoff = 0;
+                //while (curParent != null)
                 //{
-                //    _offmap.Add(0);
+                //    curParent.Value = curParent.Value.Remove(curoff + this._off, this._len);
+                //    curParent.Value = curParent.Value.Insert(curoff + this._off, repl);
+                //    curoff += curParent._off;
+                //    curParent = curParent.Parent;
                 //}
-                throw new ReplacementException(ex);
             }
-            return new Selection(selval, this, start - off);
+            this._len += q;
         }
     }
 }
--- RgxC/Program.cs
+++ RgxC/Program.cs
@@ -11,30 +11,17 @@ namespace RgxC
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test 1:");
             Selection s = new Selection("hello this is a test");
-            Selection w_hello = s.Sel(0, 5);
+            Console.WriteLine(s.Value);
             Selection w_this = s.Sel(6, 4);
-            Selection w_is = s.Sel(11, 2);
-            w_this.Replace("banana");
-            Console.WriteLine(s.Value);//hello banana is a test
-            w_this.Replace("r");
-            Console.WriteLine(s.Value);//hello r is a test
-            w_is.Replace("shall be");
-            Console.WriteLine(s.Value);//hello r shall be a test
-            Selection test = s.Sel(19, 4);
-            test.Replace("cookie");
-            Console.WriteLine(s.Value);//hello r shall be a cookie
-            test.Replace("cookies + food");
-            Console.WriteLine(s.Value);//hello r shall be a cookies + food
-            w_hello.Replace("bye");
-            Console.WriteLine(s.Value);//bye r shall be a cookies + food
-
-            s.Sel(5, 0).Replace("ee");
-            Console.WriteLine(s.Value);//expect: bye ree shall be a cookies+food //FAIL --> byeee r shall be a cookies + food
-
-            s.Sel(25, 0).Replace(" + meat");
-            Console.WriteLine(s.Value);//(theoretically)hello r shall be a cookies + food + meat--> ReplacementException
+            Selection w_a = s.Sel(14, 1);
+            w_this.Replace("rahaha");
+            Console.WriteLine(s.Value);
+            Selection ha = w_this.Sel(2, 2);
+            ha.Replace("hee");
+            Console.WriteLine(s.Value);
+            w_a.Replace("the");
+            Console.WriteLine(s.Value);
         }
     }
 }
